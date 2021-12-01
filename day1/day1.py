56a46cc113610905b7126847d4692c7b2782b791
roryk <rory.kirchner@gmail.com>
2021-12-01 16:45:42 -0500

    Do second part of day 1.

--- day1/day1.py
+++ day1/day1.py
@@ -11,3 +11,17 @@ with open("data/input.txt") as in_handle:
             increased += 1
         prev = current
 print(increased)
+
+import toolz as tz
+
+increased = 0
+with open("data/input.txt") as in_handle:
+    measurements = [int(x.strip()) for x in in_handle.readlines()]
+
+prev = math.inf
+for window in tz.sliding_window(3, measurements):
+    current = sum(window)
+    if current > prev:
+        increased += 1
+    prev = current
+print(increased)
